260ba6c7ee2f594314d54bfa9428ea80122f0446
Caio Pivetta <caiopivetta6@gmail.com>
2023-02-09 11:11:12 +0100

    Project Created

---
+++ src/main/java/com/caiopivetta6/RistorantRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.caiopivetta6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RistorantRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RistorantRegisterApplication.class, args);
+	}
+
+}
